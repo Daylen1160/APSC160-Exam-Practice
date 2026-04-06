07e4e6512c47ee60abc4889715f4df5fd8640eac
Daylen1160 <daylen924@gmail.com>
2026-04-06 00:28:26 -0700

    change

--- MT2/Hard/Game Of Life/template.h
+++ MT2/Hard/Game Of Life/template.h
@@ -5,7 +5,10 @@
  */
 
 void gameOfLife(int NUMROWS, int NUMCOLS, int board[][NUMCOLS]);
-
+void copyBoard(int NUMROWS, int NUMCOLS, int [][NUMCOLS], int [][NUMCOLS]);
+int getLiveNeighbors(int NUMROWS, int NUMCOLS, int board[][NUMCOLS], int row, int col);
+int liveCellNextState(int);
+int deadCellNextState(int);
 /**
  * @brief Update the board to the next state according to the Game of Life rules
  * @param[in] NUMROWS    Number of rows in the board
@@ -13,5 +16,44 @@ void gameOfLife(int NUMROWS, int NUMCOLS, int board[][NUMCOLS]);
  * @modifies board to its next state
  */
 void gameOfLife(int NUMROWS, int NUMCOLS, int board[][NUMCOLS]) {
-    // TODO: Implement this method
+    int newBoard[NUMROWS][NUMCOLS];
+    copyBoard(NUMROWS, NUMCOLS, board, newBoard);
+    for (int i = 0; i < NUMROWS; i++) {
+        for (int j = 0; j < NUMCOLS; j++) {
+            int liveNeighbors = getLiveNeighbors(NUMROWS, NUMCOLS, board, i, j);
+            if (board[i][j]) newBoard[i][j] = liveCellNextState(liveNeighbors);
+            else newBoard[i][j] = deadCellNextState(liveNeighbors);
+        }
+    }
+    copyBoard(NUMROWS, NUMCOLS, newBoard, board);
+}
+
+void copyBoard(int NUMROWS, int NUMCOLS, int board[][NUMCOLS], int newBoard[][NUMCOLS]){
+    for (int i = 0; i < NUMROWS; i++) {
+        for (int j = 0; j < NUMCOLS; j++) {
+            newBoard[i][j] = board[i][j];
+        }
+    }
+}
+
+int getLiveNeighbors(int NUMROWS, int NUMCOLS, int board[][NUMCOLS], int row, int col) {
+    int liveNeighbors = 0;
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            if (!(i == 0 && j == 0) && row + i >= 0 && row + i < NUMROWS && col + j >= 0 && col + j < NUMCOLS)  {
+                liveNeighbors += board[row + i][col + j];
+            }
+        }
+    }
+    return liveNeighbors;
+}
+
+int liveCellNextState(int numAlive) {
+    if (numAlive < 2 || numAlive > 3) return 0;
+    return 1;
+}
+
+int deadCellNextState(int numAlive) {
+    if (numAlive == 3) return 1;
+    return 0;
 }
