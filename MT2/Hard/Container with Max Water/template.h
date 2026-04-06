7c9d6e80706aced85b88d5bf781cbe7655a117ff
Daylen1160 <daylen924@gmail.com>
2026-04-05 23:58:29 -0700

    change

--- MT2/Hard/Container with Max Water/template.h
+++ MT2/Hard/Container with Max Water/template.h
@@ -3,8 +3,11 @@
  * @brief Container with Max Water Template
  * @author Daylen Chun
  */
+#include <math.h>
 
 int containerWithMaxWater(int heights[], int heightSize);
+int twoPointersSolution(int height[], int heightSize);
+int bruteForceSolution(int height[], int heightSize);
 
 /**
  * @brief Find the maximum water that can be contained between two heights
@@ -13,5 +16,36 @@ int containerWithMaxWater(int heights[], int heightSize);
  * @return The maximum water that can be contained by two heights
  */
 int containerWithMaxWater(int height[], int heightSize) {
-    // TODO: Implement this function
+    // SET TO 2 for TWO POINTERS SOLUTION
+    // SET TO 1 for BRUTE FORCE SOLUTION
+    int choice = 2;
+    return (choice == 2) ? twoPointersSolution(height, heightSize) : bruteForceSolution(height, heightSize);
+}
+
+int twoPointersSolution(int height[], int heightSize) {
+    int maxArea = 0;
+    int leftPointer = 0;
+    int rightPointer = heightSize - 1;
+
+    while (leftPointer < rightPointer) {
+        int lowerColumn = fmin(height[rightPointer], height[leftPointer]);
+        int currentArea = lowerColumn * (rightPointer - leftPointer);
+
+        maxArea = fmax(currentArea, maxArea);
+
+        if (lowerColumn == height[leftPointer]) leftPointer++;
+        else rightPointer--;
+    }
+    return maxArea;
+}
+
+int bruteForceSolution(int height[], int heightSize) {
+    int maxArea = 0;
+    for (int i = 0; i < heightSize-1; i++) {
+        for (int j = i+1; j < heightSize; j++) {
+            int currentArea = fmin(height[i], height[j]) * (j - i);
+            if (currentArea > maxArea) maxArea = currentArea;
+        }
+    }
+    return maxArea;
 }
